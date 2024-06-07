3b2be8a89831df375f8fef3ba5676d5b64a27ffe
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-06-07 15:47:21 -0400

    impl repl

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/joshuahenriques/cixac-interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Cixac programming language!\n",
+		user.Username)
+	fmt.Printf("Type commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
