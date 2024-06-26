bec5fd2d50031dd73ba1260dfb71f91234d2f6a0
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-06-26 17:27:21 -0400

    add support to evaluate array literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -53,6 +53,13 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
 
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 
