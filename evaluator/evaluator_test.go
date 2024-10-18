e720a92a1d662de17390e5bce62fd516f82b488f
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-10-18 11:01:11 -0400

    fix: add missing test cases for identifiers in forinloop tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -402,8 +402,11 @@ func TestForInLoopStatements(t *testing.T) {
 		expected int64
 	}{
 		{`let j = 0; for (i, ele in [1, 2, 3]) { j += i + ele }; j`, 9},
+		{`let arr = [1, 2, 3]; let j = 0; for (i, ele in arr) { j += i + ele }; j`, 9},
 		{`let j = 0; for (key, val in {0: 1, 1: 2, 2: 3}) { j += key + val}; j`, 9},
+		{`let obj = {0: 1, 1: 2, 2: 3}; let j = 0; for (key, val in obj) { j += key + val}; j`, 9},
 		{`let j = 0; for (i, ch in "string") { j += i; }; j`, 15},
+		{`let str = "string"; let j = 0; for (i, ch in str) { j += i; }; j`, 15},
 	}
 
 	for i, tt := range tests {
